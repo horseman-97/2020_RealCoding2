1f141c53ca0daf80c94beb4efd4d736b34e4648d
DoHyun Nam <ndhyun3847@ajou.ac.kr>
2020-08-03 17:53:39 +0900

    My printf HW

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    fscanf(stdin, "%d", &a);
+    if(a>0)
+        fprintf(stderr, "Error. It is positive\n");
+    else 
+        fprintf(stderr, "Error. It is negative\n");
+    fprintf(stdout, "Hello World %-d\n", a);
+}
